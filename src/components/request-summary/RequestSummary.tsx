cf7545a496b54185ff3e1ed1c276c09706f6760b
vivekkeyvalue <87976183+vivekkeyvalue@users.noreply.github.com>
2022-08-26 22:47:19 +0530

    Dev (#3)
    * Add basic layout
    
    * Minor changes
    
    * minor fixes
    
    * Add request summary card

--- src/components/request-summary/RequestSummary.tsx
+++ src/components/request-summary/RequestSummary.tsx
@@ -1,3 +1,5 @@
+import Button from 'components/button/Button';
+import { ButtonType } from 'components/button/types';
 import { FC } from 'react';
 import { RequestDetails } from 'types';
 
@@ -10,16 +12,22 @@ interface RequestSummaryProps {
   selectedRow?: RequestDetails;
 }
 
-const RequestSummary: FC<RequestSummaryProps> = ({ selectedRow, onClose }) => {
-
+const RequestSummary: FC<RequestSummaryProps> = ({ onClose }) => {
   return (
-    <div className={`rounded-[16px] shadow-lg p-[18px] w-[493px] relative bg-white simple_animation`}>
+    <div className={`rounded-[16px] shadow-lg p-[18px] w-[793px] relative bg-white simple_animation`}>
       <div className='absolute top-[10px] right-[10px] cursor-pointer' role='presentation' onClick={onClose}>
         <img src='icons/close-icon.svg' alt='Close icon' />
       </div>
       <div className='px-[21px] flex flex-col pt-[24px] justify-start'>
         <div className='text-4xl text-[#3E8C31] font-bold flex justify-start align-middle text-center'>
-          <div>{selectedRow?.amount}</div>
+          <div>Destination: Munar</div>
+        </div>
+        <div className='text-sm text-[black] flex flex-col mt-8'>
+          <div>PickUp : Kochi</div>
+          <div>Amount : Rs.25000</div>
+          <div className='mt-4'>
+            <Button type={ButtonType.BLUE}>Upload pdf</Button>
+          </div>
         </div>
       </div>
     </div>
